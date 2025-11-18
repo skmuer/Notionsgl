1b4a3f7ded02c9a41b1d86e22eb5062ae6e6c94f
skmuer <skmuer@gmail.com>
2025-11-18 15:51:16 +0800

    Update URLs in config.js for info cards

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -51,10 +51,10 @@ const CONFIG = {
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
+  HEO_INFO_CARD_URL2: 'https://github.com',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
-  HEO_INFO_CARD_TEXT3: '了解更多',
+  HEO_INFO_CARD_URL3: 'https://sanmusen.shop:443//user/Login/add?UserName=PBZ5GV&ParentID=23976&ChildPlace=1',
+  HEO_INFO_CARD_TEXT3: '购买链接',
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
