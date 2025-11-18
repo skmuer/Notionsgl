84821f1670b56f60451f3cd2d59acffccd5e65a4
skmuer <skmuer@gmail.com>
2025-11-18 13:29:02 +0800

    Update site creation date and hero section titles

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -4,7 +4,7 @@ const CONFIG = {
 
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2025-10-25', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   /*HEO_NOTICE_BAR: [
@@ -18,12 +18,12 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '定延春',
+  HEO_HERO_TITLE_2: '逆龄|抗衰|延寿',
+  HEO_HERO_TITLE_3: '461203.XYZ',
+  HEO_HERO_TITLE_4: '定延春|自体干细胞再生系统',
+  HEO_HERO_TITLE_5: '12周生物年龄逆转6.2岁|停服一月扔持续年轻',
+  HEO_HERO_TITLE_LINK: 'https://461203.xyz',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
