45eaf7a239ce89eb8d24042054258800c9078b35
skmuer <skmuer@gmail.com>
2025-11-18 22:55:53 +0800

    Update blog keywords in configuration

--- blog.config.js
+++ blog.config.js
@@ -18,7 +18,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Stone', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '定延春全球招商', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://461203.xyz', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '三姆森, 定延春,逆龄抗衰,自体干细胞再生', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
   BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
